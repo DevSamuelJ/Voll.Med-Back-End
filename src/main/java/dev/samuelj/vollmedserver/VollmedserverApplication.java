8b1cb2855f0a81a7b4a72003c59e965ace02e088
devsamuelj <samuel701silva@gmail.com>
2026-04-13 21:35:33 -0300

    build: Iniciando dependencias do projeto

---
+++ src/main/java/dev/samuelj/vollmedserver/VollmedserverApplication.java
@@ -0,0 +1,13 @@
+package dev.samuelj.vollmedserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VollmedserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VollmedserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/samuelj/vollmedserver/VollmedserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.samuelj.vollmedserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VollmedserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
